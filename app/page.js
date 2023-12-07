57ccb52a9f49f938a4d10495f4e217fa7e2df2c6
lyaaronSAIT <130510117+lyaaronSAIT@users.noreply.github.com>
2023-12-07 03:09:37 -0700

    update code

--- app/page.js
+++ app/page.js
@@ -7,6 +7,12 @@ function App() {
     return (
         <div className='flex flex-col items-center'>
             <h1 className='text-9xl mb-4'>MovieVerse</h1>
+            <h2 className='text-4xl'>placeholder: Search Bar</h2>
+            <div className='flex p-4 space-x-10 text-2xl'>
+              <h2>placeholder: View To Watch List</h2>
+              <h2>placeholder: View Favorite List</h2>
+              <h2>placeholder: Create List</h2>
+            </div>
             <div className='flex'>
                     <Popular/>
                     <TopRated/>
